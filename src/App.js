67ab6121e111b2d49c097cc57f0a9698e2343def
devjoshixo <djoshi923@gmail.com>
2023-04-02 17:58:55 +0530

    Heading done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import classes from './App.module.css';
+import TaskAdder from './components/TaskAdder';
 
 function App() {
   return (
     <div className={classes['App']}>
-      <h1 className={classes['mainHeading']}>JUST DO IT</h1>
+      <h1 className={classes['mainHeading']}>
+        Just do it.<span className={classes['mainHeading123']}></span>
+      </h1>
+
+      {/* TaskAdding  */}
+      <TaskAdder />
+      {/* Showing current tasks */}
     </div>
   );
 }
---
+++ src/components/TaskAdder.js
@@ -0,0 +1,12 @@
+import classes from './TaskAdder.module.css';
+
+const TaskAdder = () => {
+  return (
+    <section className={classes.taskadder}>
+      <input placeholder="Add items..." className={classes.textbox}></input>
+      <button className={classes.action}>I Got This!</button>
+    </section>
+  );
+};
+
+export default TaskAdder;
