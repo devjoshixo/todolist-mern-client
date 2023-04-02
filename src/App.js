1b2f9732043bb0bbf304595cef1de2e8f0785b2f
devjoshixo <djoshi923@gmail.com>
2023-04-02 18:12:46 +0530

    Heading done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import classes from './App.module.css';
 import TaskAdder from './components/TaskAdder';
+import GitRef from './components/UI/GitRef';
 
 function App() {
   return (
     <div className={classes['App']}>
+      <GitRef />
       <h1 className={classes['mainHeading']}>
         Just do it.<span className={classes['mainHeading123']}></span>
       </h1>
